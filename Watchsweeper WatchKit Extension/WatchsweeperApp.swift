73577a8ee3c6a78898caa65f2563143a89dc7c6a
Dani <example@example.com>
2022-04-08 17:50:50 +0200

    Initial Commit

---
+++ Watchsweeper WatchKit Extension/WatchsweeperApp.swift
@@ -0,0 +1,19 @@
+//
+//  WatchsweeperApp.swift
+//  Watchsweeper WatchKit Extension
+//
+//  Created by Daniel Ortega on 8/4/22.
+//
+
+import SwiftUI
+
+@main
+struct WatchsweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
